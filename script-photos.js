43eca20f6ed52dd4732dfc0cab5273e21a9897c0
Anouchka <anouchka.maurer@protonmail.com>
2021-12-08 11:42:15 +0100

    modification photos

--- script-photos.js
+++ script-photos.js
@@ -1,6 +1,7 @@
 //SLIDER SCRIPT
 let slideIndex = 1;
 showSlides(slideIndex);
+showSlidesM(slideIndex);
 
 function showSlides(n) {
   let i;
@@ -36,4 +37,51 @@ showSlides(slideIndex += n);
 // Thumbnail image controls
 function currentSlide(n) {
 showSlides(slideIndex = n);
+}
+
+function showSlidesM(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides fadeM");
+  let dots = document.getElementsByClassName("dot");
+  
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  
+  for (i = 0; i < dots.length; i++) {
+   dots[i].className = dots[i].className.replace(" active", "");
+  }
+  
+ slides[slideIndex-1].style.display = "block";
+ dots[slideIndex-1].className += " active";
+  
+ }
+
+// Next/previous controls
+function plusSlides(n) {
+showSlidesM(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+showSlidesM(slideIndex = n);
+}
+
+
+//RESPONSIVE FOOTER
+function myFunction() {
+  const x = document.getElementById("bottomNavbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
 }
